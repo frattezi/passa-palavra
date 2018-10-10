570246f2e9ed77df84e358aee2ee80000ee8843b
Guilherme Willer <guilhermewiller@hotmail.com>
2018-10-10 15:36:45 -0300

    Implementa círculos nas páginas e muda endereço de img

--- js/circles.js
+++ js/circles.js
@@ -1,7 +1,7 @@
 function createCircles(){
   var r = 270;
   var m = document.createElement("img");
-  m.src = "img/profile.png";
+  m.src = "https://raw.githubusercontent.com/frattezi/passa-palavra/front-end/img/profile.png";
   document.getElementById('circle-container').appendChild(m);
   for (var i = 65; i < 91; i++) {
     var circle = document.createElement('div');
