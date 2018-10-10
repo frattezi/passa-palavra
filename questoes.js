88d1561e28c5e75e8a1cfdffb1967f8feaab35a9
wesleymou <42716264+wesleymou@users.noreply.github.com>
2018-10-09 23:51:11 -0300

    test
    test

---
+++ questoes.js
@@ -0,0 +1,17 @@
+var DB = {  
+   "dados":[  
+      {  
+         "letra":"a",
+         "questoes":[
+          {
+            "pergunta": "Legume usado como enfeite de Halloween:",
+            "resposta": "Abobora"
+          },
+            {  
+               "pergunta":"Fruta com coroa:",
+               "resposta":"Abacaxi"
+            }
+         ]
+      }
+   ]
+};
---
+++ script.js
@@ -0,0 +1,13 @@
+﻿var text = '{ "dados": [' +
+    '{ "letra": "a", "questoes": [' +
+    '{"pergunta": "Legume usado como enfeite de Halloween:", ' +
+    '"resposta": "Abobora"},' +
+    '{"pergunta": "Fruta com coroa:",' +
+    '"resposta": "Abacaxi"} ] } ] }';
+
+var obj = JSON.parse(text);
+var textoNOVO = 'texto novo';
+
+function LerPergunta() {
+    document.getElementById('question').innerHTML = DB.dados[0].questoes[0].pergunta;
+}
