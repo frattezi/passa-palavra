86dbe8b28a87c934a6664ed0490b62fca181ad0a
Frattezi <pfrattezi@gmail.com>
2018-10-11 02:10:56 -0300

    adiciona variaveis globais

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 ﻿//GlOBALS
-var CONT = 0;
+var LETTER_NUMBER = 0;
+var ERROR_COUNT = 0;
+var HIT_COUNT = 0;
 
 //US04 - Mosta a quantidade de acertos
 function ContagemPontos(acertos) {
@@ -14,12 +16,6 @@ function ContagemPontos(acertos) {
 //US04 - Vai para o menu principal
 function MenuPrincipal() {
     location.href = '../index.html';
-}
-
-//US4 - Inicia um novo jogo, seta o contador para 0
-function NovoJogo() {
-    sessionStorage.setItem("cont",0);
-    location.href = 'views/tela_jogo.html';
 }
 
 //US01
@@ -39,24 +35,27 @@ function LerPergunta(i) {
 
 function PassouAPalavra(){
     colorirCircle(3);
-    CONT++;
-    LerPergunta(CONT);
+    LETTER_NUMBER++;
+    LerPergunta(LETTER_NUMBER);
 }
 
 function colorirCircle(i){
     // se a pergunta respondida estiver certa
     if (i == 1){
-        document.getElementById("circle-"+String.fromCharCode(CONT+65)).classList.add("btn-success");
+        document.getElementById("circle-"+String.fromCharCode(LETTER_NUMBER+65)).classList.add("btn-success");
         
     // se errar
     }if(i == 2){
-        document.getElementById("circle-"+String.fromCharCode(CONT+65)).classList.add("btn-danger");
+        document.getElementById("circle-"+String.fromCharCode(LETTER_NUMBER+65)).classList.add("btn-danger");
     }
     
     // se for passa a palavra
     if (i == 3){
-        document.getElementById("circle-"+String.fromCharCode(CONT+65)).classList.add("btn-warning");
+        document.getElementById("circle-"+String.fromCharCode(LETTER_NUMBER+65)).classList.add("btn-warning");
     }
        
 }
-    
+
+function contador(i) {
+    document.getElementById("respostaFinal").innerHTML = "Voce acertou "+HIT_COUNT+" perguntas!";
+}
