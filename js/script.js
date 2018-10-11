7fa6b7dd978009f904b0f4826c3927e3bbaf42aa
Wesley <wesley.pra@hotmail.com>
2018-10-10 23:27:39 -0300

    10/10 23:27

--- js/script.js
+++ js/script.js
@@ -1,25 +1,4 @@
-﻿//US01
-//i é o numero da letra, 0=a
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-var n = getRandomInt(0, 1);
-function LerPergunta(i) {
-    var pergunta = DB.dados[i].questoes[n].pergunta;
-    document.getElementById('question').innerHTML = pergunta;
-    return pergunta;
-}
-//US04
-function MenuPrincipal() {
-    location.href = '../index.html';
-}
-
-var cont=0;
-function NovoJogo() {
-    location.href = 'views/tela_jogo.html';
-    cont=0;
-}
-//US04
+﻿//US04 - Mosta a quantidade de acertos
 function ContagemPontos(acertos) {
     var contagem = document.getElementById('contagem');
     if (acertos == 1) {
@@ -27,4 +6,31 @@ function ContagemPontos(acertos) {
     } else {
         contagem.innerHTML = 'Você acertou ' + acertos + ' perguntas!';
     }
+}
+
+//US04 - Vai para o menu principal
+function MenuPrincipal() {
+    location.href = '../index.html';
+}
+
+//US4 - Inicia um novo jogo, seta o contador para 0
+function NovoJogo() {
+    sessionStorage.setItem("cont",0);
+    location.href = 'views/tela_jogo.html';
+}
+
+//US01
+//Resgata o valor do contador
+cont = sessionStorage.getItem("cont");
+//i é o numero da letra, 0=a
+function LerPergunta(i) {
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    var n = getRandomInt(0, 1);
+
+    var pergunta = DB.dados[i].questoes[n].pergunta;
+    document.getElementById('question').innerHTML = pergunta;
+    return pergunta;
 }
